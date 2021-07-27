d267d34b20669a22b67067576bc6944c38bfa70c
Christopher D Couture <videogameroulette@yahoo.ca>
2021-07-26 22:22:49 -0400

    initial commit

---
+++ SRTPluginUIDinoCrisis1DirectXOverlay/PluginInfo.cs
@@ -0,0 +1,26 @@
+﻿using SRTPluginBase;
+using System;
+
+namespace SRTPluginUIDinoCrisis1DirectXOverlay
+{
+    internal class PluginInfo : IPluginInfo
+    {
+        public string Name => "DirectX Overlay UI (Dino Crisis 1)";
+
+        public string Description => "A DirectX-based Overlay User Interface for displaying Dino Crisis 1 game memory values.";
+
+        public string Author => "VideoGameRoulette";
+
+        public Uri MoreInfoURL => new Uri("https://github.com/SpeedrunTooling/SRTPluginUIDinoCrisis1DirectXOverlay");
+
+        public int VersionMajor => assemblyFileVersion.ProductMajorPart;
+
+        public int VersionMinor => assemblyFileVersion.ProductMinorPart;
+
+        public int VersionBuild => assemblyFileVersion.ProductBuildPart;
+
+        public int VersionRevision => assemblyFileVersion.ProductPrivatePart;
+
+        private System.Diagnostics.FileVersionInfo assemblyFileVersion = System.Diagnostics.FileVersionInfo.GetVersionInfo(System.Reflection.Assembly.GetExecutingAssembly().Location);
+    }
+}
